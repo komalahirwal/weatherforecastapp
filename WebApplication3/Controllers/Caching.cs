2021195de7adf11a4c22323d7bd46586e0a9cf0a
komal.ahirwal <komal.ahirwal@SL-000006>
2023-08-17 13:14:08 +0530

    Add project files.

---
+++ WebApplication3/Controllers/Caching.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net.Http.Headers;
+using System.Web.Http.Filters;
+
+namespace WebApplication3.Controllers
+{
+    public class Caching: System.Web.Http.Filters.ActionFilterAttribute
+    {
+        public int TimeDuration { get; set; }
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                MaxAge = TimeSpan.FromSeconds(TimeDuration),
+                MustRevalidate = true,
+                Public = true
+            };
+        }
+    }
+}
